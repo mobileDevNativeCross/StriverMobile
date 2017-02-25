3331f556b28f75f384ae650fb46af5b0519f677d
Dan <dan.jakaitis@gmail.com>
2017-02-25 11:02:47 -0500

    Mock Fetch Request - now with headers

--- src/services/randomNumberService.js
+++ src/services/randomNumberService.js
@@ -1,19 +1,30 @@
+import store from '../redux/store';
 
 export async function generateRandomNumber() {
 
-  //sample api call - this is 401, need to add the jwt to header as bearer
-  fetch('https://strivermobile-api.herokuapp.com/api/home/next')
+   //HACK JUST FOR TESTING PURPOSE
+  var id_token = store.getState()._root.entries[0][1]._root.entries[2][1]._root.entries[2][1];
+
+  console.log(id_token);
+
+  //sample api call - THIS IS NOT HOW WE SHOULD DO THIS, JUST FOR TESTING PURPOSES
+  fetch('https://strivermobile-api.herokuapp.com/api/home/next',{
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + id_token
+    }
+  })
   .then((response) => {
-    //console.log(state.authenticationToken);
-    console.log(response);
+    return response.json();
   })
   .then((responseJson) => {
-    console.log(responseJson);  
-  })  
-  .catch((e)=>{
-      alert(e);
+    console.log(responseJson);
+  })
+  .catch((e) => {
+    console.log(e);
   });
 
+  //for the demo of initial app (pepperoni) - can remove
   return new Promise((res) => setTimeout(res, 1000))
     .then(() => Math.floor(Math.random() * 100));
 }
