cb5e2f7e9e8620fa88e7fcac337cb5bf037e9251
Roman <romangalynov@gmail.com>
2017-04-18 11:47:32 +0300

    Fix backandoid button

--- src/modules/AppView.js
+++ src/modules/AppView.js
@@ -24,8 +24,7 @@ const AppView = React.createClass({
   propTypes: {
     isReady: PropTypes.bool.isRequired,
     isLoggedIn: PropTypes.bool.isRequired,
-    // dispatch: PropTypes.func.isRequired,
-    // containerGetPrevNavigationState: PropTypes.func.isRequired,
+    containerGetPrevNavigationState: PropTypes.func.isRequired,
   },
 
   componentDidMount() {
@@ -35,7 +34,6 @@ const AppView = React.createClass({
     .then(token => {
       if (!token) {
         auth0.showLogin();
-        // dispatch(SessionStateActions.initializeSessionState());
       } else {
         console.log('checking old token');
       }
@@ -57,24 +55,7 @@ const AppView = React.createClass({
     //   .catch(error => console.warn('snapshotUtil.resetSnapshot() error', error));
   },
 
-  // componentWillReceiveProps({isReady, isLoggedIn}){
-  //   if (!this.props.isReady) {
-  //     if (isReady && !isLoggedIn) {
-  //       auth0.showLogin();
-  //     }
-  //   }
-  // },
-
-
   render() {
-    // if (!this.props.isReady) {
-    //   return (
-    //     <View style={{flex: 1}}>
-    //       <ActivityIndicator style={styles.centered}/>
-    //     </View>
-    //   );
-    // }
-    // console.warn(this.props.isReady);
     return (
       <View style={styles.container}>
         {
